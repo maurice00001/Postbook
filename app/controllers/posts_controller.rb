89ef4a0665e431955c4101081bb2215e34b21bda
Maurice Spanninga <maurice00001@hotmail.com>
2012-10-30 00:33:31 +0100

    Generated Profile controller, styled flash notices

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+ before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :delete]
+ 
   # GET /posts
   # GET /posts.json
   def index
