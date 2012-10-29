c2592d3e63372bc9629a1298b4a3424afb6d0d4d
Maurice Spanninga <maurice00001@hotmail.com>
2012-10-29 16:57:57 +0100

    Scaffolding generated, Bootstrap installed, index edited

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Postbook::Application.config.secret_token = '843047dbc8884df45744cf4ebefc8cc7c9e06d79807a43668cbefefef46c3d7e6f2977516088605cfe82838877db28d1cb57d905fe6084da7acd974be03965eb'
