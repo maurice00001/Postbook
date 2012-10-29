3d1b13f3bbce0ad93fe1baa81d62933fd807210f
Maurice Spanninga <maurice00001@hotmail.com>
2012-10-29 20:01:52 +0100

    Installed devise, add user, generate devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Postbook::Application.routes.draw do
+  devise_for :users
+
   resources :posts
+  root :to => 'posts#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
